3284a7276d96689a0c1ce69328a51ce68c2a660c
AA2653 <38747223+AA2653@users.noreply.github.com>
2018-05-01 03:20:58 +0800

    Update player.js

--- backend/processor/player.js
+++ backend/processor/player.js
@@ -1,7 +1,7 @@
 const CONSTS = require('../constants')
 
 function processPlayer (buf, actor, repObj, waitingHandle, dataOut, iteration) {
-	//console.log(waitingHandle);
+  // console.log（'processAPawn handle ='，waitingHandle，actor.guid，buf.remainingBits）
   switch (waitingHandle) {
     case 1:
       if (buf.readBit()) {
